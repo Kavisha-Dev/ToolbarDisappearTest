d034d5816c60e49846a26056c6cb802fa82ec977
kavisha <kavisha_jg@yahoo.com>
2021-10-04 20:46:12 +0530

    Initial Commit

---
+++ Shared/ToolbarDisappearTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarDisappearTestApp.swift
+//  Shared
+//
+//  Created by Kavisha Sonaal on 04/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarDisappearTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
